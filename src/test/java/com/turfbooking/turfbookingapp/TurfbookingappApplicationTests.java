c09dd2a509ffb554ff6b302e5c219397b516fcc3
Tecson-7 <txn3108@gmail.com>
2026-02-04 09:51:16 +0530

    Step 1: Spring Boot project initialized

---
+++ src/test/java/com/turfbooking/turfbookingapp/TurfbookingappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turfbooking.turfbookingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurfbookingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
